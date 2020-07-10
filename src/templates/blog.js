7acbee86bd7d8991822cc16f63247d473e9bccb0
coolbotic <jabarson@live.com>
2020-07-10 13:11:04 +0100

    safari fix 6

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -15,13 +15,14 @@ const BlogTemplate = props => {
       <div className="blogTempateWrapper">
         <div className="blogTempateBox">
           <div className="blogHeader">
-            <div
+            
+            <div className="blogInfo">
+              <div
               className="blogHero"
               style={{
                 backgroundImage: `url(${props.data.contentfulBlog.featuredImage.fluid.src})`,
               }}
             ></div>
-            <div className="blogInfo">
               <div className="blogHeaderBorder">
                 <h1 className="blogTempateLables blogTempateTitle">
                   {props.data.contentfulBlog.title}
